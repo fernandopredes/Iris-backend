e43f7a5394eb321f062947c6c1ec6f3d4a4e739c
fernando predes <fernando_predes@hotmail.com>
2023-11-14 00:09:23 -0300

    add pytest

---
+++ tests/test_model.py
@@ -0,0 +1,29 @@
+import pytest
+from sklearn.metrics import accuracy_score
+from ml_model.model import train_model, load_model, predict_iris
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+@pytest.fixture
+def iris_dataset():
+    iris = load_iris()
+    X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=7)
+    return X_train, X_test, y_train, y_test
+
+def test_model_accuracy(iris_dataset):
+    X_train, X_test, y_train, y_test = iris_dataset
+
+    # Treinar e salvar o modelo
+    train_model()
+
+    # Carregar o modelo treinado
+    model = load_model()
+
+    # Fazer previsões no conjunto de teste
+    predictions = model.predict(X_test)
+
+    # Calcular a precisão
+    accuracy = accuracy_score(y_test, predictions)
+
+    # Verificar se a precisão atende ao threshold definido
+    assert accuracy > 0.90
